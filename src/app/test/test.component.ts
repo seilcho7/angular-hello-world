654f43ca913c533b8517b7e51acec4de59141997
Seil Cho <seilcho7@hotmail.com>
2019-07-16 15:34:13 -0400

    adds function

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
       Hello {{name}}
     </h2>
     <h2>{{1+1}}</h2>
-    <h2>{{"Welcome" + name}}</h2>
+    <h2>{{"Welcome " + name}}</h2>
     <h2>{{name.length}}</h2>
     <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{greetUser()}}</h2>
+    <h2>{{siteUrl}}</h2>
     `,
   // styleUrls: ['./test.component.css']
   styles: [`
@@ -22,9 +24,14 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public name = "Seil";
+  public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser() {
+    return "Hello " + this.name;
   }
 
 }
