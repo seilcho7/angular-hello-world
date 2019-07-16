8b4b458f215315ff14ce3cf17b64b0724e94835a
Seil Cho <seilcho7@hotmail.com>
2019-07-16 14:49:23 -0400

    adds inline template and styles

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: '[app-test]',
+  // templateUrl: './test.component.html',
+  template: `<div>
+              Inline template
+            </div>`,
+  // styleUrls: ['./test.component.css']
+  styles: [`
+    div {
+      color: red;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
