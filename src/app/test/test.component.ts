11ec7bd7442fe0cf66d646d6112dc640119b28c2
Seil Cho <seilcho7@hotmail.com>
2019-07-16 15:29:50 -0400

    learn template interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   // templateUrl: './test.component.html',
-  template: `<div>
-              Inline template
-            </div>`,
+  template: `
+    <h2>
+      Hello {{name}}
+    </h2>
+    <h2>{{1+1}}</h2>
+    <h2>{{"Welcome" + name}}</h2>
+    <h2>{{name.length}}</h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    `,
   // styleUrls: ['./test.component.css']
   styles: [`
     div {
@@ -15,6 +21,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public name = "Seil";
   constructor() { }
 
   ngOnInit() {
