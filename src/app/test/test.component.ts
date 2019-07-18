5f25543166d4ca90dbb77ac53ecd3068cfb21ade
Seil Cho <seilcho7@hotmail.com>
2019-07-18 10:55:53 -0400

    learn property binding in angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-test]',
-  // templateUrl: './test.component.html',
-  template: `
-    <h2>
-      Hello {{name}}
-    </h2>
-    <h2>{{1+1}}</h2>
-    <h2>{{"Welcome " + name}}</h2>
-    <h2>{{name.length}}</h2>
-    <h2>{{name.toUpperCase()}}</h2>
-    <h2>{{greetUser()}}</h2>
-    <h2>{{siteUrl}}</h2>
-    `,
+  templateUrl: './test.component.html',
+  // template: `
+  //   <h2>
+  //     Hello {{name}}
+  //   </h2>
+  //   <h2>{{1+1}}</h2>
+  //   <h2>{{"Welcome " + name}}</h2>
+  //   <h2>{{name.length}}</h2>
+  //   <h2>{{name.toUpperCase()}}</h2>
+  //   <h2>{{greetUser()}}</h2>
+  //   <h2>{{siteUrl}}</h2>
+
+  //   `,
   // styleUrls: ['./test.component.css']
   styles: [`
     div {
@@ -24,6 +25,8 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public name = "Seil";
+  public myId = "testId";
+  public isDisabled = true;
   public siteUrl = window.location.href;
   constructor() { }
 
